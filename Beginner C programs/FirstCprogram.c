5c610c84a04dec2ec3b3abbe97607c1d461754a0
Masanso David <masadavid010@gmail.com>
2026-05-14 21:59:22 +0300

    Add newline at the end of FirstCprogram.c
    Ensure file ends with a newline for consistency.

--- Beginner C programs/FirstCprogram.c
+++ Beginner C programs/FirstCprogram.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main() {
+  // Writing a banking program that allows you to withdraw amount if only balance (in integer) is above 5000.00.
+  int minAmount = 5000;
+  int withdraw;
+  printf("Please, enter the amount to withdraw: \n");
+  scanf("%d", &withdraw);
+  
+  
+  
+}
