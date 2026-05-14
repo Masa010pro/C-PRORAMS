4ac9f936155f63e29f0395ffbc03b6fb6af0af31
Masanso David <masadavid010@gmail.com>
2026-05-14 22:09:06 +0300

    Update withdrawal logic and fix print statements
    Fix logic for withdrawal message and correct format specifier.

--- Beginner C programs/FirstCprogram.c
+++ Beginner C programs/FirstCprogram.c
@@ -3,9 +3,22 @@ int main() {
   // Writing a banking program that allows you to withdraw amount if only balance (in integer) is above 5000.00.
   int minAmount = 5000;
   int withdraw;
+  
   printf("Please, enter the amount to withdraw: \n");
   scanf("%d", &withdraw);
-  
+
+  // Logic to check the amount.
+
+  if(withdraw > minAmount) {
+    
+    printf("Withdraw is processing, please wait!\n);
+      printf("Your cash of ugx %d is ready, please pick it.", withdraw);
+    
+  }
+  else{
+    printf("Your account balance is too low, %d. Please recharge and try again.", %d);
+  }
+  return 0;
   
   
 }
